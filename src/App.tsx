1c3e243a7df5465f3cbbd510b281b5723896f31b
Jose Vasconcelos <tadeo199928j@gmail.com>
2025-05-20 21:42:17 +0100

    New rect-web

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import MovieCard from "./components/MovieCard"
+
+
+function App() {
+  return (
+    <>
+      <MovieCard movie={{title: "Tim's Film",release_date:"2024",url: "empty"}}/>
+
+    </>
+  );
+
+}
+
+export default App
